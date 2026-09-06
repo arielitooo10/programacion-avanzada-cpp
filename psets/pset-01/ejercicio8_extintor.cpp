b778ebda9b513097c38f9ed08bcbe3c1ec38c195
arielitooo10 <arielalejandro1713@gmail.com>
2026-09-06 17:07:02 -0500

    Actualizar ejercicio8_extintor.cpp

--- psets/pset-01/ejercicio8_extintor.cpp
+++ psets/pset-01/ejercicio8_extintor.cpp
@@ -1,17 +1,17 @@
 // Ejercicio 8: ExtintorIncendios
-//
+// 
 // Completa la clase ExtintorIncendios: un atributo privado presionPSI y
 // un constructor ExtintorIncendios(double presionInicial). Si
 // presionInicial es menor o igual a 0, o mayor a 200, el constructor
 // avisa por consola ("Presion invalida, se asigna presion segura por
 // defecto") y asigna 100.0 como valor seguro por defecto. Si es valida,
 // la asigna directamente. Agrega tambien el getter getPresionPSI().
-//
+// 
 // Salida esperada:
 // Presion: 150
 // Presion invalida, se asigna presion segura por defecto
 // Presion: 100
-//
+// 
 // Compilar:  g++ -std=c++20 -Wall -Wextra -g ejercicio8_extintor.cpp -o bin/ejercicio8
 // Ejecutar:  ./bin/ejercicio8
 
@@ -26,6 +26,12 @@ public:
         // TODO: si presionInicial es invalida (<= 0 o > 200), imprime el
         // aviso y asigna presionPSI = 100.0. Si no, asigna presionPSI =
         // presionInicial.
+        if (presionInicial <= 0.0 || presionInicial > 200.0) {
+            std::cout << "Presion invalida, se asigna presion segura por defecto" << std::endl;
+            presionPSI = 100.0;
+        } else {
+            presionPSI = presionInicial;
+        }
     }
 
     double getPresionPSI() {
@@ -42,3 +48,4 @@ int main() {
 
     return 0;
 }
+
