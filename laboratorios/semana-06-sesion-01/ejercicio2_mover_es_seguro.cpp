fd7e588709e3bf5fc57ee1f0565316a298346e68
arielitooo10 <arielalejandro1713@gmail.com>
2026-09-21 13:10:48 +0000

    Agrega constructor y operador de asignacion de movimiento (move semantics)

--- laboratorios/semana-06-sesion-01/ejercicio2_mover_es_seguro.cpp
+++ laboratorios/semana-06-sesion-01/ejercicio2_mover_es_seguro.cpp
@@ -2,57 +2,75 @@
 #include <utility>
 
 class RegistroDeVuelo {
-    private:
-        double* alturas;
-        int capacidad;
-    public:
-        RegistroDeVuelo(int nuevaCapacidad) {
-            capacidad = nuevaCapacidad;
-            alturas = new double[capacidad];
-            std::cout << "Registro de vuelo creado para " << capacidad << " lecturas" << std::endl;
+private:
+double* alturas;
+int capacidad;
+public:
+RegistroDeVuelo(int nuevaCapacidad) {
+capacidad = nuevaCapacidad;
+alturas = new double[capacidad];
+std::cout << "Registro de vuelo creado para " << capacidad << " lecturas" << std::endl;
         }
 
         // TODO: constructor de movimiento. Recibe RegistroDeVuelo&& otro,
         // roba su puntero "alturas" y su "capacidad" (sin reservar memoria
         // nueva ni copiar ningun elemento), y deja "otro" en un estado
         // vacio y seguro (alturas = nullptr, capacidad = 0).
+RegistroDeVuelo(RegistroDeVuelo&& otro) {
+alturas = otro.alturas;
+capacidad = otro.capacidad;
+otro.alturas = nullptr;
+otro.capacidad = 0;
+std::cout << "Moviendo registro (capacidad " << capacidad << ")" << std::endl;
+        }
 
         // TODO: operador de asignacion de movimiento. Misma idea que el
         // constructor de movimiento, pero primero libera con delete[] la
         // memoria que este objeto ya tenia, antes de robar la de "otro".
         // Recuerda comprobar "this != &otro" antes de liberar.
-
-        void guardarAltura(int indice, double valor) {
-            alturas[indice] = valor;
-        }
-
-        double getAltura(int indice) {
-            if (alturas == nullptr) {
-                std::cout << "Registro vacio (fue movido)" << std::endl;
-                return 0.0;
+RegistroDeVuelo& operator=(RegistroDeVuelo&& otro) {
+if (this != &otro) {
+delete[] alturas;
+alturas = otro.alturas;
+capacidad = otro.capacidad;
+otro.alturas = nullptr;
+otro.capacidad = 0;
             }
-            return alturas[indice];
+std::cout << "Moviendo (asignacion) registro (capacidad " << capacidad << ")" << std::endl;
+return *this;
         }
 
-        ~RegistroDeVuelo() {
-            std::cout << "Destruyendo registro (capacidad " << capacidad << ")" << std::endl;
-            delete[] alturas;
+void guardarAltura(int indice, double valor) {
+alturas[indice] = valor;
+        }
+
+double getAltura(int indice) {
+if (alturas == nullptr) {
+std::cout << "Registro vacio (fue movido)" << std::endl;
+return 0.0;
+            }
+return alturas[indice];
+        }
+
+~RegistroDeVuelo() {
+std::cout << "Destruyendo registro (capacidad " << capacidad << ")" << std::endl;
+delete[] alturas;
         }
 };
 
 int main() {
-    RegistroDeVuelo registro1(5);
-    registro1.guardarAltura(0, 120.0);
+RegistroDeVuelo registro1(5);
+registro1.guardarAltura(0, 120.0);
 
-    RegistroDeVuelo registro2 = std::move(registro1);
-    registro2.guardarAltura(0, 999.0);
+RegistroDeVuelo registro2 = std::move(registro1);
+registro2.guardarAltura(0, 999.0);
 
-    std::cout << "registro1.getAltura(0) = " << registro1.getAltura(0) << std::endl;
-    std::cout << "registro2.getAltura(0) = " << registro2.getAltura(0) << std::endl;
+std::cout << "registro1.getAltura(0) = " << registro1.getAltura(0) << std::endl;
+std::cout << "registro2.getAltura(0) = " << registro2.getAltura(0) << std::endl;
 
-    RegistroDeVuelo registro3(3);
-    registro3 = std::move(registro2);
-    std::cout << "registro3.getAltura(0) = " << registro3.getAltura(0) << std::endl;
+RegistroDeVuelo registro3(3);
+registro3 = std::move(registro2);
+std::cout << "registro3.getAltura(0) = " << registro3.getAltura(0) << std::endl;
 
-    return 0;
+return 0;
 }
